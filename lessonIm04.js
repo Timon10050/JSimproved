cdb9b25e07060e55892be27ac6b909b6b57ffcdf
Artem Belyaev <arthouse98@gmail.com>
2022-02-10 00:29:11 +0300

    Go go gogit add .

--- lessonIm04.js
+++ lessonIm04.js
@@ -1,17 +1,34 @@
 'use strict';
 
-let text = prompt('Введите текст:');
+let text = prompt('Введите текст :)');
 
 function getText(res) {
     if (typeof res !== 'string') {
         alert('Данные не верны, дай текст кожаный!!!', text);
     } else {
-        res.trim();
+        res = res.trim();
         if (res.length > 30) {
-            res.substring(0, 30) + '...';
+            res = res.substring(0, 30) + '...';
         }
         return res;
     }
 }
 
 console.log(getText(text));
+
+
+/* 2й вариант */
+function getText(res) {
+    if (typeof res !== 'string') {
+        alert('Данные не верны, последняя попытка!!!');
+    } else {
+        res = res.trim();
+        if (res.length > 30) {
+            res = res.substring(0, 30) + '...';
+        }
+        return res;
+    }
+}
+
+console.log(getText(123123));
+console.log(getText(' Иван Дулин Дулю Дулил, Надулился и ссутутулился растроился - ушел. '));
